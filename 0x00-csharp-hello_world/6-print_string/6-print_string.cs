cb576e2ccd111013089b026a8f0a7fc082365f10
LauraPeralta85 <lalaperalta85@gmail.com>
2020-04-13 17:39:20 -0500

    [feature] print repeated string

---
+++ 0x00-csharp-hello_world/6-print_string/6-print_string.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+            string str = "Holberton School";
+            string res = str.Split(' ')[0];;
+            Console.Write("{0}{0}{0}" + "\n", str);
+            Console.WriteLine(res);
+    }
+}
