e50a36e89d8e7f261206e2cc71cb2804d6a9d516
LauraPeralta85 <lalaperalta85@gmail.com>
2020-04-14 14:07:02 -0500

    [feature] print underscore line

---
+++ 0x01-csharp-ifelse_loops_methods/10-print_line/10-print_line.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Line
+{
+    public static void PrintLine(int length)
+    {
+        int i;
+        if (length <= 0) {
+            Console.Write("\n");
+        }
+        else {
+            for (i = 0; i <= length; i++) {
+                Console.Write("_");
+            }
+            Console.WriteLine("");
+        }
+    }
+}
