eb46ff4836de6f12b5e6d04adb94a5a67a3a4e3e
LauraPeralta85 <lalaperalta85@gmail.com>
2020-04-14 14:26:47 -0500

    [feature] print diagonal

---
+++ 0x01-csharp-ifelse_loops_methods/11-print_diagonal/11-print_diagonal.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Line
+{
+    public static void PrintDiagonal(int length)
+    {
+        int i, j;
+        if (length <= 0) {
+            Console.Write("\n");
+        }
+        else {
+            for (i = 1; i <= length; i++) {
+                
+                for (j = 0; j < i; j++) {
+                    Console.Write(" ");   
+                } 
+                Console.WriteLine("\\");   
+            }
+            Console.WriteLine("");
+        }
+    }
+}
