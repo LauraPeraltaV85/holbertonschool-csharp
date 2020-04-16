2e1fc05aa40b4fc8e1afb5c7aa43f0d9f0f6ae74
LauraPeralta85 <lalaperalta85@gmail.com>
2020-04-16 14:56:59 -0500

    [feature] print int list

---
+++ 0x02-csharp-arrays_lists_dictionaries/4-print_list/4-print_list.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<int> CreatePrint(int size)
+    {
+        int i;
+        List<int> newList = new List<int> ();
+        if (size < 0) {
+              Console.WriteLine("Size cannot be negative");
+            return null;
+        }
+        for (i = 0; i < size; i++) {
+            newList.Add(i);
+            if (i < size - 1) {
+                Console.Write(newList[i] + " ");
+            }
+            else {
+                Console.WriteLine(newList[i]);
+            }
+        }
+        return newList;
+    }
+}
