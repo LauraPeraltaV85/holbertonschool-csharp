59c4d864a8bcdd2b8e67f87a7cb33b383a41965c
LauraPeralta85 <lalaperalta85@gmail.com>
2020-04-16 15:26:09 -0500

    [feature] find max int in list

---
+++ 0x02-csharp-arrays_lists_dictionaries/5-max_integer/5-max_integer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static int MaxInteger(List<int> myList)
+    {
+        if (myList.Count == 0){
+            Console.WriteLine("List is empty");
+            return -1;
+        }
+        int i = 1;
+        int maxVal = myList[0];
+        while (i < myList.Count) {
+            if (myList[i] > maxVal) {
+                maxVal = myList[i];
+            }
+            i++;
+        }
+        return maxVal;
+    } 
+}
