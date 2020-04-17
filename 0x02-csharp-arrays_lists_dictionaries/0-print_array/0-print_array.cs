e6669dfabed0583862d8d2c9a2f0bf0368cac95c
LauraPeralta85 <lalaperalta85@gmail.com>
2020-04-16 21:12:36 -0500

    [fix] statement order

--- 0x02-csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
+++ 0x02-csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -5,11 +5,11 @@ class Array
     public static int[] CreatePrint(int size)
     {
         int i;
-        int[] length = new int[size];
         if (size < 0) {
             Console.WriteLine("Size cannot be negative");
             return null;
         }
+        int[] length = new int[size];
         for (i = 0; i < size; i++) {
             length[i] = i;
             if (i < size - 1) {
