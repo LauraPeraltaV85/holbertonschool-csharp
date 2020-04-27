684433d1104fe9005960f40b646e7154faeb38fa
LauraPeralta85 <lalaperalta85@gmail.com>
2020-04-27 14:27:27 -0500

    [fix] double quote at printing

--- 0x03-csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
+++ 0x03-csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
@@ -18,7 +18,7 @@ class MyStack
         }
         if (aStack.Contains(search))
         {
-            Console.WriteLine("Stack contains {0}: True", search);
+            Console.WriteLine("Stack contains \"{0}\": True", search);
             while (aStack.Contains(search))
             {
                 aStack.Pop();
