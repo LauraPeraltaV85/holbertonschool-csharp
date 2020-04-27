4a8d29c758fd1975058d83f64ba1bde33071068c
LauraPeralta85 <lalaperalta85@gmail.com>
2020-04-27 17:08:56 -0500

    [feature] remove first node and return its value

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/9-linkedlist_pop/9-linkedlist_pop.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static int Pop(LinkedList<int> myLList)
+    {
+        LinkedListNode<int> head = new LinkedListNode<int>(myLList.First.Value);
+        if (myLList.Count == 0)
+        {
+            return 0;
+        }
+        else 
+        {
+           myLList.RemoveFirst();
+           return head.Value; 
+        }
+    }
+}
