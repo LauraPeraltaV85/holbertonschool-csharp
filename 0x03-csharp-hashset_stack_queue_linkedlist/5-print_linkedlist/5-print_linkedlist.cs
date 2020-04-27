64df388d3bfca5c82591f8d43c3e7fe74af2ba51
LauraPeralta85 <lalaperalta85@gmail.com>
2020-04-27 15:46:13 -0500

    [feature] create and print linked list

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/5-print_linkedlist/5-print_linkedlist.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static LinkedList<int> CreatePrint(int size)
+    {
+        int[] numbers = new int[size];
+        for (int i = 0; i < size; i++)
+        {
+            numbers[i] = i;
+            Console.WriteLine(numbers[i]);
+        }
+        LinkedList<int> list = new LinkedList<int>(numbers);        
+        return list;
+    }
+}
