d2104c372e3e2194e0f7fb9eddda2743aaddf043
LauraPeralta85 <lalaperalta85@gmail.com>
2020-04-27 16:02:18 -0500

    [feature] list length without using .count

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/6-linkedlist_length/6-linkedlist_length.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static int Length(LinkedList<int> myList)
+    {
+        int i = 0;
+        foreach (int index in myList)
+        {
+            i++;
+        }
+        return i;
+    }
+}
