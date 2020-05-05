c27e59ebca50ffe0620eff5e2a7c84f3f87074d6
LauraPeralta85 <lalaperalta85@gmail.com>
2020-05-05 12:09:04 -0500

    [feature] method add of two integers and its tests

--- 0x07-csharp-tdd/0-add/MyMath.Tests/MyMath.Tests.cs
+++ 0x07-csharp-tdd/0-add/MyMath.Tests/MyMath.Tests.cs
@@ -2,9 +2,11 @@ using NUnit.Framework;
 
 namespace MyMath.Tests
 {
+    ///<summary> represents the tests for Operations Class </summary>
     [TestFixture]
     public class Operations_Tests
     {
+        /// <sumary> test Add method from MyMath Class </sumary>
         [Test]
         [TestCase(5, 10)]
         [TestCase(-5, 8)]
