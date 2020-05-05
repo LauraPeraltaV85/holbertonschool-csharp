04b6e65e0e138953bd35ee799e31101e32d6b1f5
LauraPeralta85 <lalaperalta85@gmail.com>
2020-05-05 16:26:47 -0500

    [fix] go back to first approach with testCase

--- 0x07-csharp-tdd/0-add/MyMath.Tests/MyMath.Tests.cs
+++ 0x07-csharp-tdd/0-add/MyMath.Tests/MyMath.Tests.cs
@@ -8,16 +8,14 @@ namespace MyMath.Tests
     {
         /// <sumary> test Add method from MyMath Class </sumary>
         [Test]
-        // [TestCase(5, 10)]
-        // [TestCase(-5, 8)]
-        // [TestCase(0, 4)]
-        // [TestCase(1000, 2)]
-        // [TestCase(0, 0)]
-        // [TestCase(-4, -10)]
-        public void Add_whenTwoNumbersPassed_ReturnsSumResult()
+        [TestCase(5, 10)]
+        [TestCase(-5, 8)]
+        [TestCase(0, 4)]
+        [TestCase(1000, 2)]
+        [TestCase(0, 0)]
+        [TestCase(-4, -10)]
+        public void Add_whenTwoNumbersPassed_ReturnsSumResult(int a, int b)
         {
-            int a = 3;
-            int b = 0;
             int result = Operations.Add(a, b);
             Assert.AreEqual(a + b, result);
         }
