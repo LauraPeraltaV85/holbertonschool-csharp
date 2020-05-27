0c866ad0517e284508c3e9e94276b863686afbf1
LauraPeralta85 <lalaperalta85@gmail.com>
2020-05-26 19:02:42 -0500

    [fix] condition

--- 0x09-csharp-linear_algebra/20-matrix_rotate_2D/20-matrix_rotate_2D.cs
+++ 0x09-csharp-linear_algebra/20-matrix_rotate_2D/20-matrix_rotate_2D.cs
@@ -25,10 +25,10 @@ class MatrixMath
                 Console.WriteLine(matrix[x, y] + "===" + matrix[x, y+1]);
                 //Console.WriteLine(angleSin + "===" + angleCos);
                 Console.WriteLine((matrix[x, y] * angleCos) + "===" + (matrix[x, y + 1] * (angleSin)));
-                //matrix[x, y] = Math.Round((matrix[x, y] * angleCos) - (matrix[x, y + 1] * (angleSin)), 2);
-                //matrix[x, y + 1] = Math.Round((matrix[x, y] * angleSin) + (matrix[x, y + 1] * (angleCos)), 2);
-                matrix[x, y] = Math.Round((matrix[x, y] * (angleCos)) - (matrix[x, y + 1] * (angleSin)), 2);
-                matrix[x, y + 1] = Math.Round((matrix[x, y] * (angleSin)) + (matrix[x, y + 1] * (angleCos) + 1), 2);
+                matrix[x, y] = Math.Round((matrix[x, y] * angleCos) - (matrix[x, y + 1] * (angleSin)), 2);
+                matrix[x, y + 1] = Math.Round((matrix[x, y] * angleSin) + (matrix[x, y + 1] * (angleCos)), 2);
+                // matrix[x, y] = (matrix[x, y] * (angleCos)) - (matrix[x, y + 1] * (angleSin));
+                // matrix[x, y + 1] = (matrix[x, y] * (angleSin)) + (matrix[x, y + 1] * (angleCos) + 1);
             }
         }
         return matrix;
